03034c1a6ae8581289a4a8229607eaba97aa48ec
josepCortesManyanich <gaara96@gmail.com>
2024-04-08 06:17:25 +0200

    API SERIES solved

--- src/views/Series.jsx
+++ src/views/Series.jsx
@@ -5,6 +5,7 @@ import axios from 'axios'
  export default function Series() {
     
     const[series, setSeries] = useState(null)
+    const[series1, setSeries1] = useState(null)
 
     useEffect (() => {
         const data = async () => {
@@ -27,6 +28,28 @@ import axios from 'axios'
         }
         data()
     },[])
+
+    useEffect (() => {
+        const data1 = async () => {
+            const options = {
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/trending/tv/day?language=en-US',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDMyNTc0YTQzZTMyZTAwNGFmNWVhMmJjZDU5MjdlZiIsInN1YiI6IjY2MTFhYjgzMTEwOGE4MDE2NDhjN2VlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sVMODQOs97ekq4hGuXhLckNoM4_OM9Fj9yqomyzwcZ4'
+                }
+              };
+            try {
+             const response = await axios(options)
+             setSeries1(response.data.results)
+             console.log(response.data.results)
+
+            } catch (error) {
+                console.error(error)
+            }
+        }
+        data1()
+    },[])
   
   
   
@@ -47,6 +70,20 @@ import axios from 'axios'
                   </div>
                 </li> )
         })}
+
+        {series1 && series1.map( elemento => {
+                return( 
+                    <li key={elemento.id} className='item'> 
+                        <div className='contenido'>
+                            <img   src={`https://image.tmdb.org/t/p/w500/${elemento.poster_path}`} alt="" /> 
+                            <p>{elemento.name}</p>
+                                <div className='buttons'>
+                                    <button className='buttonMg'></button>
+                                    <button className='buttonVisto'></button>
+                                </div>
+                        </div>
+                        </li> )
+                })}
         </ul>
 
       
