1683473c972b0401d5dd9a6ce9e7f39c54ae7f43
josepCortesManyanich <gaara96@gmail.com>
2024-04-08 05:55:28 +0200

    SERIES WIP

--- src/views/Series.jsx
+++ src/views/Series.jsx
@@ -8,23 +8,47 @@ import axios from 'axios'
 
     useEffect (() => {
         const data = async () => {
+            const options = {
+                method: 'GET',
+                url: 'https://api.themoviedb.org/3/tv/airing_today?language=en-US&page=1',
+                headers: {
+                  accept: 'application/json',
+                  Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDMyNTc0YTQzZTMyZTAwNGFmNWVhMmJjZDU5MjdlZiIsInN1YiI6IjY2MTFhYjgzMTEwOGE4MDE2NDhjN2VlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sVMODQOs97ekq4hGuXhLckNoM4_OM9Fj9yqomyzwcZ4'
+                }
+              };
             try {
-             const response = await axios.get('http://localhost:8000/api/v1/series')
+             const response = await axios(options)
              setSeries(response.data.results)
-             console.log(response.data)
+             console.log(response)
 
             } catch (error) {
                 console.error(error)
             }
         }
         data()
-    })
+    },[])
   
   
   
   
     return (
     <div>
+        <ul className='list'>
+        {series && series.map( elemento => {
+           return( 
+              <li key={elemento.id} className='item'> 
+                  <div className='contenido'>
+                      <img   src={`https://image.tmdb.org/t/p/w500/${elemento.poster_path}`} alt="" /> 
+                      <p>{elemento.name}</p>
+                        <div className='buttons'>
+                            <button className='buttonMg'></button>
+                            <button className='buttonVisto'></button>
+                         </div>
+                  </div>
+                </li> )
+        })}
+        </ul>
+
       
     </div>
   )
