7fcc6b3552f8181810430479ba3a634abd06f2b7
josepCortesManyanich <gaara96@gmail.com>
2024-04-09 13:07:51 +0200

    more or less

--- src/views/MoviesDetail.jsx
+++ src/views/MoviesDetail.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
+import Navbar from '../components/Navbar'
 
  export default function MoviesDetail({match}) {
     const{id} = useParams();
@@ -20,7 +21,8 @@ import axios from 'axios'
                     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDMyNTc0YTQzZTMyZTAwNGFmNWVhMmJjZDU5MjdlZiIsInN1YiI6IjY2MTFhYjgzMTEwOGE4MDE2NDhjN2VlMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sVMODQOs97ekq4hGuXhLckNoM4_OM9Fj9yqomyzwcZ4'
                 },
             });
-            setDetail(response.data.result)
+            console.log(response.data)
+            setDetail(response.data)
         } catch (error) {
             console.error(error)
         }
@@ -32,9 +34,42 @@ import axios from 'axios'
   
   return (
     <div>
-      
+    <Navbar/>
+    <div className='detalle'>
+        
+         {movieDetail && 
+                <li key={movieDetail.id} className='item1'>
+                    <div  className='contenido1'>
+                    <img   src={`https://image.tmdb.org/t/p/w500/${movieDetail.poster_path}`} alt="" /> 
+                      <p>{movieDetail.title}</p>
+                    </div>
+                    <div className='contenido2'>
+                        <p> Año de estreno:{movieDetail.release_date}</p>
+                        <p>Descripción:{movieDetail.overview}</p>
+                        {movieDetail.genres && (
+                            <div className='contenido2'>
+                                <p> Generos:</p>
+                                <ul className='lista-genero'>
+                                    {movieDetail.genres.map(elemento => (
+                                        <li key={elemento.id} className='item-genero'>{elemento.name}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        )}
+                        
+                    
+                        <div className='buttons1'>
+                            <button className='buttonMg1'></button>
+                            <button className='buttonVisto1'></button>
+                         </div>
+                    </div>
+                </li>
+            }
+        
+    </div>
     </div>
   )
 }
+
 
 
